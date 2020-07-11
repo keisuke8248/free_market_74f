a804f95a76078725d55b91ac6455bb13a029486d
kotatunikurumaru <takumi.suzuki0411@gmail.com>
2020-07-12 00:45:42 +0900

    readmeを追記しました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :age, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
     end
   protect_from_forgery with: :exception
 
