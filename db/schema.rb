fef70804ff31e052fb6329848251915f2cb6f927
hiroshikado <okonomiyakunarahiroimono@yahoo.co.jp>
2020-07-31 15:42:10 +0900

    product-list1

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,8 @@ ActiveRecord::Schema.define(version: 2020_07_08_122342) do
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
+    t.bigint "buyer_id"
+    t.bigint "seller_id", null: false
     t.bigint "category_id", null: false
     t.bigint "brand_id", null: false
     t.string "name", null: false
@@ -76,17 +77,15 @@ ActiveRecord::Schema.define(version: 2020_07_08_122342) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["brand_id"], name: "index_products_on_brand_id"
+    t.index ["buyer_id"], name: "index_products_on_buyer_id"
     t.index ["category_id"], name: "index_products_on_category_id"
-    t.index ["user_id"], name: "index_products_on_user_id"
+    t.index ["seller_id"], name: "index_products_on_seller_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.string "email", null: false
+    t.string "user_password", null: false
     t.string "user_image"
     t.string "family_name", null: false
     t.string "first_name", null: false
@@ -96,8 +95,6 @@ ActiveRecord::Schema.define(version: 2020_07_08_122342) do
     t.text "introduction"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "cards", "users"
@@ -105,5 +102,6 @@ ActiveRecord::Schema.define(version: 2020_07_08_122342) do
   add_foreign_key "images", "products"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
-  add_foreign_key "products", "users"
+  add_foreign_key "products", "users", column: "buyer_id"
+  add_foreign_key "products", "users", column: "seller_id"
 end
