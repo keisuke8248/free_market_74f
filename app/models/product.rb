a55c49350b38f7002c0a43e39490b4942859d340
yukiochiai <ochiai.y41@gmail.com>
2020-08-03 15:46:40 +0900

    endのつけ忘れ

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,5 +8,4 @@ class Product < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
   validates :seller_id, :name, :description, :status, :category_id, :prefecture_id, :cost, :days, :price, presence: true
-
- 
+end
