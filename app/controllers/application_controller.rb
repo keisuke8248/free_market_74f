760f389a5f97d2e059e5593b10406924939eeb24
JACK15280 <junya1053@gmail.com>
2020-08-17 22:38:27 +0900

    トップページのマークアップ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_parents
 
+  def set_parents
+    @parents = Category.where(ancestry: nil)
+  end
   protected
 
     def configure_permitted_parameters
