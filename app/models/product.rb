9038d317c14848bc5b86b0996a25538731ef85a6
yukiochiai <ochiai.y41@gmail.com>
2020-09-04 22:54:21 +0900

    商品出品マークアップ修正と追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,19 +8,9 @@ class Product < ApplicationRecord
   has_many :images, dependent: :destroy
   has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
-  validate :images_sheets
   validates :seller_id, :name, :description, :status, :category_id, :prefecture_id, :cost, :days, :price, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
 
-  def images_sheets
-    if images.present?
-      if images.length > 10
-        errors.add(:image, '10枚まで投稿できます')
-      end      
-    else
-      errors.add(:image, '画像がありません')
-    end
-  end
 end
 
   def text_placeholder
