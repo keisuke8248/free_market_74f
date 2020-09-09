d3f1be75cb7f106ddf6cc7a0c48ef6bdba0fe4f8
ino-tomo <inotomo.8711@gmail.com>
2020-09-09 18:51:22 +0900

    商品情報編集機能の実装③

--- app/assets/javascripts/edit_products.js
+++ app/assets/javascripts/edit_products.js
@@ -3,41 +3,35 @@ window.addEventListener('DOMContentLoaded', function(){
   var dataBox = new DataTransfer();
 
   const inputElement = document.getElementsByClassName('image-preview')
-  const previewElement = document.getElementsByClassName('qqq')
+  const previewElement = document.getElementsByClassName('img-preview')
 
   for( var i=0; i<inputElement.length; i++) {
       inputElement[i].addEventListener("change", event => {
-        event.preventDefault();     // デフォルトイベントのキャンセル
-        event.stopPropagation();    // イベントのバブリングを防ぐ
+        event.preventDefault();
+        event.stopPropagation();
 
-        // type="file"を指定されたinput要素のchangeイベントは「ファイルのリスト」を返す
         const file = event.target.files[0];
-
-        // FileReaderのインスタンスを生成（ローカルファイルを読み込むオブジェクト）
         const reader = new FileReader();
         const data_index = Number(event.target.getAttribute("data-index"))
-        // FileReaderの読み込みが完了した結果（アップロードした画像ファイルのデータ）を、img要素のsrcにセット
+
         reader.addEventListener('load', event => {
-          // event.target.resultは、base64エンコードされた画像データ
           previewElement[data_index].setAttribute('src', event.target.result);
         })
 
-        // セットされたオブジェクトを読み込む
         reader.readAsDataURL(file);
       });
     };
 
   $('#new-file').change(function(){
-    //選択したfileのオブジェクトをpropで取得
+
     var file = $('input[type="file"]').prop('files')[0];
     $.each(this.files, function(i, file){
-    //FileReaderのreadAsDataURLで指定したFileオブジェクトを読み込む
-    file_field = this;
-    // file = this.files[0]
-    // console.log(file)
-    var fileReader = new FileReader();
-    //読み込みが完了すると、srcにfileのURLを格納
-    dataBox.items.add(file)
+
+      file_field = this;
+
+      var fileReader = new FileReader();
+
+      dataBox.items.add(file)
     
     file_field.files = dataBox.files
     
@@ -52,7 +46,6 @@ window.addEventListener('DOMContentLoaded', function(){
                       </div>
                     </div>
                   </div>`
-      //image_box__container要素の前にhtmlを差し込む
       $('#previews').after(html);
     };
     });
